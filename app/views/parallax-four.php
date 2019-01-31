fa3c5364e6239de233cfdac61939579b41c9328a
alissonsilva12 <alisson.sousa12@gmail.com>
2019-01-31 13:11:35 -0300

    alterando o frame de ingressos

--- app/views/parallax-four.php
+++ app/views/parallax-four.php
@@ -2,7 +2,7 @@
     <div class="section no-pad-bot">
         <div class="container">
             <div class="row center">
-                <h2 class="header col s12 light text-parallax"><strong>INSCREVA-SE!</strong></h2>
+            
                 <!--<div id="sympla-widget-288555"></div> -->
                 <!--<a target="_blank" href="http://bit.ly/rapaduraconf" class="waves-effect waves-light btn-large color-marron">Clique aqui para comprar o seu ingresso</a>-->
 
