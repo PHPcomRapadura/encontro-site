58001978f94b3a8e7b4fb6d762cfa4777aade155
AlissonSilvaCe <alisson.sousa12@gmail.com>
2019-02-26 21:12:09 -0300

    ajustando o widget de ingressos

--- app/views/parallax-four.php
+++ app/views/parallax-four.php
@@ -1,17 +1,15 @@
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
+
         <div class="container">
             <div class="row center">
-            
-                <!--<div id="sympla-widget-288555"></div> -->
-                <!--<a target="_blank" href="http://bit.ly/rapaduraconf" class="waves-effect waves-light btn-large color-marron">Clique aqui para comprar o seu ingresso</a>-->
 
-                <div id="sympla-widget-433123" height="auto"></div> 
+                <div class="row center" id="sympla-widget-433123" height="auto"></div> 
                 
                 <script src="https://www.sympla.com.br/js/sympla.widget-pt.js/433123"></script>
-            
             </div>
+
         </div>
-    </div>
-    <div class="parallax"><img class="img-parallax" src="app/public/assets/img/parallax-four.jpg" alt="Unsplashed background img 2"></div>
-</div>
+
+
+
+
+
