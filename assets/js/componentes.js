b2e51808765248f411ac2fd7d78baab0457c56a8
Chicão Thiago <fthiagogv@gmail.com>
2025-03-29 17:45:21 -0300

    feat(speakers): ✨ adicionando palestrantes erandir e jose

--- assets/js/componentes.js
+++ assets/js/componentes.js
@@ -1,101 +1,101 @@
 function createTableRow(time, activity, facilitator) {
-    // Create the table row element
-    const tr = document.createElement('tr');
-    tr.classList.add('table--row');
+  // Create the table row element
+  const tr = document.createElement('tr');
+  tr.classList.add('table--row');
 
-    // Create the time cell
-    const th = document.createElement('th');
-    th.scope = 'row';
-    th.classList.add('table--fixed-cell');
-    th.textContent = time;
-    tr.appendChild(th);
+  // Create the time cell
+  const th = document.createElement('th');
+  th.scope = 'row';
+  th.classList.add('table--fixed-cell');
+  th.textContent = time;
+  tr.appendChild(th);
 
-    // Create the activity cell
-    const tdActivity = document.createElement('td');
-    tdActivity.classList.add('table--cell');
-    tdActivity.textContent = activity;
-    tr.appendChild(tdActivity);
+  // Create the activity cell
+  const tdActivity = document.createElement('td');
+  tdActivity.classList.add('table--cell');
+  tdActivity.textContent = activity;
+  tr.appendChild(tdActivity);
 
-    // Create the facilitator cell
-    const tdFacilitator = document.createElement('td');
-    tdFacilitator.classList.add('table--cell');
-    tdFacilitator.textContent = facilitator;
-    tr.appendChild(tdFacilitator);
+  // Create the facilitator cell
+  const tdFacilitator = document.createElement('td');
+  tdFacilitator.classList.add('table--cell');
+  tdFacilitator.textContent = facilitator;
+  tr.appendChild(tdFacilitator);
 
-    return tr;
+  return tr;
 }
 
 const initScheduleTable = (schedule) => {
-    // Get the table body element
-    const tableBody = document.getElementById('schedule-table');
+  // Get the table body element
+  const tableBody = document.getElementById('schedule-table');
 
-    // Create a table row for each agenda item
-    schedule.map(({ horario, atividade, facilitador }) => {
-        const newRow = createTableRow(horario, atividade, facilitador);
-        tableBody.appendChild(newRow);
-    });
+  // Create a table row for each agenda item
+  schedule.map(({ horario, atividade, facilitador }) => {
+    const newRow = createTableRow(horario, atividade, facilitador);
+    tableBody.appendChild(newRow);
+  });
 };
 
 
 const speakersComponent = {
-    generateSpeakerCard: (palestrante, isModal = false) => {
-        const { nome, foto, titulo, miniBio } = palestrante;
-        const nameId = nome.toLowerCase().replace(' ', '-');
+  generateSpeakerCard: (palestrante, isModal = false) => {
+    const { nome, foto, titulo, miniBio } = palestrante;
+    const nameId = nome.toLowerCase().replace(' ', '-');
 
-        // Create the outer container div
-        const containerDiv = document.createElement('div');
+    // Create the outer container div
+    const containerDiv = document.createElement('div');
 
-        // Create the card content div
-        const cardContentDiv = document.createElement('div');
-        cardContentDiv.className = 'card--content h-[calc(100%-32px)]';
-        cardContentDiv.id = `'speaker-${nameId}'`;
+    // Create the card content div
+    const cardContentDiv = document.createElement('div');
+    cardContentDiv.className = 'card--content h-[calc(100%-32px)]';
+    cardContentDiv.id = `'speaker-${nameId}'`;
 
-        // Add the title
-        const titleDiv = document.createElement('div');
-        const title = document.createElement('p');
-        title.className = 'card--title';
-        title.textContent = nome;
-        titleDiv.appendChild(title);
-        cardContentDiv.appendChild(titleDiv);
+    // Add the title
+    const titleDiv = document.createElement('div');
+    const title = document.createElement('p');
+    title.className = 'card--title';
+    title.textContent = nome;
+    titleDiv.appendChild(title);
+    cardContentDiv.appendChild(titleDiv);
 
-        // Add the image
-        const imageDiv = document.createElement('div');
-        const image = document.createElement('img');
-        image.src = `assets/img/speakers/${foto}`;
-        image.className = isModal ? 'w-fit m-auto' : 'm-auto w-fit max-h-[410px]';
-        imageDiv.appendChild(image);
-        cardContentDiv.appendChild(imageDiv);
+    // Add the image
+    const imageDiv = document.createElement('div');
+    const image = document.createElement('img');
+    image.src = `assets/img/speakers/${foto}`;
+    image.className = isModal ? 'w-fit m-auto' : 'm-auto w-fit max-h-[410px]';
+    imageDiv.appendChild(image);
+    cardContentDiv.appendChild(imageDiv);
 
-        // Add the talk description
-        const talkDescription = document.createElement('p');
-        talkDescription.className = 'text-md';
-        talkDescription.innerHTML = `<span class="font-bold">Palestra: </span>${titulo}`;
-        cardContentDiv.appendChild(talkDescription);
+    // Add the talk description
+    const talkDescription = document.createElement('p');
+    talkDescription.className = 'text-md';
+    talkDescription.innerHTML = `<span class="font-bold">Palestra: </span>${titulo}`;
+    cardContentDiv.appendChild(talkDescription);
 
-        // Add the mini-bio
-        const miniBioElement = document.createElement('p');
-        miniBioElement.className = `text-xs ${isModal ? '' : 'line-clamp-3'}`;
-        miniBioElement.innerHTML = `<span class="font-bold">Mini-bio:</span>${miniBio} `;
-        cardContentDiv.appendChild(miniBioElement);
+    // Add the mini-bio
+    const miniBioElement = document.createElement('p');
+    miniBioElement.className = `text-xs ${isModal ? '' : 'line-clamp-3'}`;
+    miniBioElement.innerHTML = `<span class="font-bold">Mini-bio: </span>${miniBio} `;
+    cardContentDiv.appendChild(miniBioElement);
 
-        // Append the card content to the container div
-        containerDiv.appendChild(cardContentDiv);
+    // Append the card content to the container div
+    containerDiv.appendChild(cardContentDiv);
 
-        if (!isModal) {
-            // Create the button
-            const button = document.createElement('button');
-            button.className = 'link--button w-full';
-            button.textContent = 'Bio completa';
-            button.setAttribute('onclick', `openModal('${nome}')`);
-            containerDiv.appendChild(button);
-        }
-
-        return containerDiv;
-    },
-    initSpeaker: (palestrantes) => {
-        palestrantes.forEach((palestrante) => {
-            const speakerCard = speakersComponent.generateSpeakerCard(palestrante);
-            document.getElementById('speakers-container').appendChild(speakerCard);
-        })
+    if (!isModal) {
+      // Create the button
+      const button = document.createElement('button');
+      button.className = 'link--button w-full';
+      button.textContent = 'Bio completa';
+      button.setAttribute('onclick', `openModal('${nome}')`);
+      containerDiv.appendChild(button);
     }
+
+    return containerDiv;
+  },
+  initSpeaker: (palestrantes) => {
+    palestrantes.forEach((palestrante) => {
+      const speakerCard = speakersComponent.generateSpeakerCard(palestrante);
+      document.getElementById('speakers-container').appendChild(speakerCard);
+    })
+  }
 }
--- assets/js/dados.js
+++ assets/js/dados.js
@@ -1,11 +1,12 @@
 const palestrantes = [
-    {
-        nome: 'Ronny John',
-        foto: 'ronny-john.JPG',
-        miniBio: 'Desenvolvedor fulllstack na Secult CE trabalhando com PHP, usando Symfony e VanilllaJS. Criador de extensões frustado pelo Whatsapp Web e vereador nos eventos de comunidades.',
-        titulo: 'Fibers e Concorrência no PHP',
-    },
-    { nome: 'Lorena Alencar', titulo: 'O Algoritmo da Desigualdade: Como a Tecnologia Pode Reforçar ou Reduzir Injustiças Sociais.', foto: 'lorena-alencar.jpg', miniBio: `<p>Oi! Sou assistente social formada desde 2022, com experiência tanto na assistência social quanto na educação. Minha trajetória profissional passou pelo CREAS, onde trabalhei com populações em situação de vulnerabilidade, e hoje atuo na Secretaria de Educação e Desporto de Guaiúba, unindo o Serviço Social à área educacional.</p>Além disso, faço trabalho voluntário na Procuradoria da Mulher de Guaiúba, contribuindo na luta pelos direitos das mulheres.
+  {
+    nome: 'Ronny John',
+    foto: 'ronny-john.JPG',
+    miniBio: 'Desenvolvedor fulllstack na Secult CE trabalhando com PHP, usando Symfony e VanilllaJS. Criador de extensões frustado pelo Whatsapp Web e vereador nos eventos de comunidades.',
+    titulo: 'Fibers e Concorrência no PHP',
+  },
+  {
+    nome: 'Lorena Alencar', titulo: 'O Algoritmo da Desigualdade: Como a Tecnologia Pode Reforçar ou Reduzir Injustiças Sociais.', foto: 'lorena-alencar.jpg', miniBio: `<p>Oi! Sou assistente social formada desde 2022, com experiência tanto na assistência social quanto na educação. Minha trajetória profissional passou pelo CREAS, onde trabalhei com populações em situação de vulnerabilidade, e hoje atuo na Secretaria de Educação e Desporto de Guaiúba, unindo o Serviço Social à área educacional.</p>Além disso, faço trabalho voluntário na Procuradoria da Mulher de Guaiúba, contribuindo na luta pelos direitos das mulheres.
         <p>No quesito formação, sou pós-graduada em:</p>
         <ul>
           <li>Direitos Humanos e Serviço Social</li>
@@ -14,29 +15,29 @@ const palestrantes = [
           <li>Gestão de Pessoas (em andamento)</li>
         </ul>
       <p>Nos próximos anos, quero expandir minha atuação na docência e continuar promovendo a inclusão e os direitos sociais. Além disso, venho explorando possibilidades na área da tecnologia, pensando em como meu conhecimento pode contribuir para soluções inovadoras e mais acessíveis. Sempre de olho em novos desafios!</p> ` },
-    { nome: 'Gabriel Camurça', foto: 'gabriel-camurca.jpg', miniBio: 'Gabriel Camurça é desenvolvedor Back-end com 3 anos de experiência em projetos web. Formado em Análise e Desenvolvimento de Sistemas e cursando pós-graduação em Arquitetura de Software. Trabalhou em um cartório desenvolvendo soluções personalizadas e, atualmente, na BR Tech, criando plataformas web sob demanda. Fortes habilidades em bancos de dados PHP, JavaScript e SQL. Além disso, é Técnico em Redes e SysAdmin Linux, com expertise em Cloud AWS e Cybersegurança. Amante da literatura, Gabriel encontrou paz na tecnologia em suas palestras.', titulo: 'C4Model para Devs', },
-    { nome: 'Pedro Arthur', foto: 'pedro-arthur.jpeg', miniBio: 'Desenvolvedor backend, SRE e pesquisador na área de Redes de Computadores e Sistemas Distribuídos. O cabra já passou por um bocado de linguagens – de C a Scala, de Java a Go – mas nunca largou o bom e velho PHP!', titulo: 'Observabilidade sem Rodeios com OpenTelemetry', },
-    { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
-    { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
-    // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
-    // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
-    // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
-    // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
+  { nome: 'Gabriel Camurça', foto: 'gabriel-camurca.jpg', miniBio: 'Gabriel Camurça é desenvolvedor Back-end com 3 anos de experiência em projetos web. Formado em Análise e Desenvolvimento de Sistemas e cursando pós-graduação em Arquitetura de Software. Trabalhou em um cartório desenvolvendo soluções personalizadas e, atualmente, na BR Tech, criando plataformas web sob demanda. Fortes habilidades em bancos de dados PHP, JavaScript e SQL. Além disso, é Técnico em Redes e SysAdmin Linux, com expertise em Cloud AWS e Cybersegurança. Amante da literatura, Gabriel encontrou paz na tecnologia em suas palestras.', titulo: 'C4Model para Devs', },
+  { nome: 'Pedro Arthur', foto: 'pedro-arthur.jpeg', miniBio: 'Desenvolvedor backend, SRE e pesquisador na área de Redes de Computadores e Sistemas Distribuídos. O cabra já passou por um bocado de linguagens – de C a Scala, de Java a Go – mas nunca largou o bom e velho PHP!', titulo: 'Observabilidade sem Rodeios com OpenTelemetry', },
+  { nome: 'Erandi Junior', foto: 'erandir-junior.jpg', miniBio: 'Erandir Junior, desenvolvedor sênior na Trust, apaixonado por boas práticas de programação. O cabra domina PHP, JavaScript e já lidou com um bocado de bancos de dados. Apesar de se apresentar como backend, também manda bem no front!', titulo: 'Trabalhando com Microsserviços da Melhor Forma. Porque Tem Muita Gente Que Não Sabe', },
+  { nome: 'José Wesley Gonçalvez', foto: 'jose-golcalves.jpg', miniBio: ' José Wesley Gonçalves, bacharel em Ciências da Computação pela UFERSA e pedreiro de códigos back-end na Invillia.', titulo: 'WSL: Uma Porta para o Mundo Linux', },
+  // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
+  // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
+  // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
+  // { nome: 'Palestrante Indefinido', foto: 'user.avif', miniBio: 'Poderia ser você aqui', titulo: 'Você tem muito a compartilhar', },
 ]
 
 const agenda = [
-    { horario: '07:30 - 08:00', atividade: 'Credenciamento', facilitador: 'Comunidade Php com Rapadura' },
-    { horario: '08:00 - 08:30', atividade: '', facilitador: '' },
-    { horario: '08:30 - 09:10', atividade: '', facilitador: '' },
-    { horario: '09:30 - 09:50', atividade: '', facilitador: '' },
-    { horario: '09:30 - 09:50', atividade: '', facilitador: '' },
-    { horario: '09:50 - 10:30', atividade: 'Coffe Break', facilitador: 'Comunidade PHP com Rapadura' },
-    { horario: '10:30 - 11:10', atividade: '', facilitador: '' },
-    { horario: '11:30 - 11:55', atividade: '', facilitador: '' },
-    { horario: '11:55 - 13:00', atividade: 'Almoço', facilitador: 'Todo mundo' },
-    { horario: '13:00 - 13:40', atividade: '', facilitador: '' },
-    { horario: '13:40 - 14:20', atividade: '', facilitador: '' },
-    { horario: '14:20 - 15:00', atividade: '', facilitador: '' },
-    { horario: '15:00 - 15:30', atividade: '', facilitador: '' },
-    { horario: '15:30 - 16:00', atividade: 'Encerramento: agradecimentos e sorteior de brindes', facilitador: 'Comunidade PHP com Rapadura' },
+  { horario: '07:30 - 08:00', atividade: 'Credenciamento', facilitador: 'Comunidade Php com Rapadura' },
+  { horario: '08:00 - 08:30', atividade: '', facilitador: '' },
+  { horario: '08:30 - 09:10', atividade: '', facilitador: '' },
+  { horario: '09:30 - 09:50', atividade: '', facilitador: '' },
+  { horario: '09:30 - 09:50', atividade: '', facilitador: '' },
+  { horario: '09:50 - 10:30', atividade: 'Coffe Break', facilitador: 'Comunidade PHP com Rapadura' },
+  { horario: '10:30 - 11:10', atividade: '', facilitador: '' },
+  { horario: '11:30 - 11:55', atividade: '', facilitador: '' },
+  { horario: '11:55 - 13:00', atividade: 'Almoço', facilitador: 'Todo mundo' },
+  { horario: '13:00 - 13:40', atividade: '', facilitador: '' },
+  { horario: '13:40 - 14:20', atividade: '', facilitador: '' },
+  { horario: '14:20 - 15:00', atividade: '', facilitador: '' },
+  { horario: '15:00 - 15:30', atividade: '', facilitador: '' },
+  { horario: '15:30 - 16:00', atividade: 'Encerramento: agradecimentos e sorteior de brindes', facilitador: 'Comunidade PHP com Rapadura' },
 ]
